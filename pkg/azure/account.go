c3c73602e36bb489039d44d2b4f3cb3b16b2bb53
Angel M. Adames <angelmadames@gmail.com>
2024-10-18 07:56:57 -0400

    chore: Add SetAccountSubscription func

--- pkg/azure/account.go
+++ pkg/azure/account.go
@@ -18,6 +18,24 @@ type AccountInfo struct {
 	} `json:"user"`
 }
 
+func SetAccountSubscription(subscriptionId string) {
+	if CurrentAccount().SubscriptionId == subscriptionId {
+		log.Info("Current account is already set to target subscription!")
+		return
+	}
+
+	cmd := []string{"az", "account", "set", "--subscription", subscriptionId}
+
+	accountSubscription := exec.Command(cmd[0], cmd[1:]...)
+	_, err := accountSubscription.Output()
+
+	if err != nil {
+		log.Fatalf("Failed to set Azure account subscription: %v", err)
+	}
+
+	log.Info("Azure accounted set!", "subscription", subscriptionId)
+}
+
 func CurrentAccount() AccountInfo {
 	cmd := []string{"az", "account", "show", "--output", "json"}
 
